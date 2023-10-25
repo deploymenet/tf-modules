2495ba3865f8c9c4d8255fd1e5d56a4f21cddc01
pooriaghaedi <pooria.ghaedi@gmail.com>
2023-10-25 10:05:29 +0300

    S3backend module added

--- s3backend/kms.tf
+++ s3backend/kms.tf
@@ -10,6 +10,6 @@ resource "aws_kms_key" "terraform-bucket-key" {
 }
 
 resource "aws_kms_alias" "key-alias" {
-  name          = "alias/terraform-bucket-key"
+  name          = "alias/terraform-s3backend-key"
   target_key_id = aws_kms_key.terraform-bucket-key.key_id
 }
